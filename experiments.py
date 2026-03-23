c1072e2f783c0d592333379cb86c3bf7b1849cb3
lukebclarke <luke616logins@gmail.com>
2026-03-23 18:55:11 +0000

    Feat: Added experiments.py

---
+++ experiments.py
@@ -0,0 +1,24 @@
+from adaptive_regressor import AdaptiveRegressor
+
+from pmlb import fetch_data, dataset_names
+import seaborn as sns
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+import gzip
+
+dataset_name = 'feynman_II_38_14'
+X, Y = fetch_data(dataset_name, return_X_y=True)
+
+est = AdaptiveRegressor(
+    pop_size=200,
+    gens=40,
+    max_time=8.0*60.0*60.0,
+    cxpb=0.7,
+    mutpb=0.1,
+    k=3,
+    functions=["+", "-", "*", "/", "sqrt", "sin", "cos", "log"],
+    verbose=True
+)
+est.fit(X, Y)
+print(est.hof[0])
