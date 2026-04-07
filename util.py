cc4f7d6b454e6e4374b849b1bb31887ecd0919a3
lukebclarke <luke616logins@gmail.com>
2026-04-07 12:05:01 +0100

    Finds average similarity of files in folder

--- util.py
+++ util.py
@@ -8,6 +8,10 @@ import pickle
 import importlib
 from daytona import Daytona
 import sys
+import os
+from itertools import combinations
+
+import copydetect
 
 from daytona.common.errors import DaytonaNotFoundError
 
@@ -80,3 +84,28 @@ def clean_llm_output(output):
         generated_code = generated_code.replace("mutuated_individual", "mutated_individual")
 
     return output
+
+def get_similarity(folder_path):
+    #Gets all files in directory
+    files = os.listdir(folder_path)
+    
+    #Finds all combinations of files
+    combos = list(combinations(files, 2))
+
+    all_similarities = []
+    for pair in combos:
+        #Finds filepaths of files
+        filepath0 = folder_path + "/" + pair[0]
+        filepath1 = folder_path + "/" + pair[1]
+
+        #Compares the similarity between files using the Winnowing algorithm
+        fp1 = copydetect.CodeFingerprint(filepath0, 25, 1)
+        fp2 = copydetect.CodeFingerprint(filepath1, 25, 1)
+        token_overlap, similarities, slices = copydetect.compare_files(fp1, fp2)
+
+        all_similarities.append(similarities[0])
+        all_similarities.append(similarities[1])
+        
+    avg_similarity = sum(all_similarities) / len(all_similarities)
+
+    return avg_similarity
